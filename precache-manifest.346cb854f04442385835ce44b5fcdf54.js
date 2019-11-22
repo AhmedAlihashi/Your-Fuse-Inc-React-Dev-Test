325edf737a0e6507e1e913f3f0fae81537b1fb0a
PremePanther <ahmedali.96@hotmail.com>
2019-11-22 16:57:29 -0500

    Updates

---
+++ precache-manifest.346cb854f04442385835ce44b5fcdf54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cc919101d5a4228c68e68589315b1b",
+    "url": "/Your-Fuse-Inc-React-Dev-Test/index.html"
+  },
+  {
+    "revision": "9d8aaae6e7d2ad8f247b",
+    "url": "/Your-Fuse-Inc-React-Dev-Test/static/css/main.796e7f74.chunk.css"
+  },
+  {
+    "revision": "ff636fff5dc83cfb13d9",
+    "url": "/Your-Fuse-Inc-React-Dev-Test/static/js/2.50ec466e.chunk.js"
+  },
+  {
+    "revision": "9d8aaae6e7d2ad8f247b",
+    "url": "/Your-Fuse-Inc-React-Dev-Test/static/js/main.9cfe632b.chunk.js"
+  },
+  {
+    "revision": "e61a91f79b5cbdee881d",
+    "url": "/Your-Fuse-Inc-React-Dev-Test/static/js/runtime-main.ca195f83.js"
+  }
+]);
