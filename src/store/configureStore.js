0389ec289769d071bdde5e014b590aeba7d0dd4f
PremePanther <ahmedali.96@hotmail.com>
2019-11-22 14:09:02 -0500

    removed redundant code

--- src/store/configureStore.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "../reducers";
-//rootReducer is the index.js file
-export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
-}
