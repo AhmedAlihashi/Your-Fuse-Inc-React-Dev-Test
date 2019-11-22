f29dcd753fc7a469315cb32c6ad5b7768499c844
PremePanther <ahmedali.96@hotmail.com>
2019-11-22 16:20:44 -0500

    moved redux functions to Data component

---
+++ src/components/Data.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { Grid, Typography, Card, CardContent } from "@material-ui/core";
+import { connect } from "react-redux";
+import * as dataAction from "../actions/dataAction";
+
+import Slider from "infinite-react-carousel";
+
+export class Data extends Component {
+  componentDidMount() {
+    this.props.loadData();
+  }
+  render() {
+    const settings = {
+      initialSlide: true,
+      slidesPerRow: 3
+    };
+    const { data } = this.props;
+
+    const dataList = data.length ? (
+      data.map(data => {
+        let x = data.supply;
+        let y = data.priceUsd;
+        let z = data.volumeUsd24Hr;
+
+        let supply = Number(x).toFixed(2);
+        let price = Number(y).toFixed(2);
+        let volume = Number(z).toFixed(2);
+        return (
+          <Grid item key={data.rank}>
+            <Card>
+              <CardContent>
+                <Grid
+                  container
+                  direction="row"
+                  justify="space-between"
+                  alignItems="center"
+                >
+                  <Typography>{data.name}</Typography>
+                  <Typography>{data.symbol}</Typography>
+                  <Typography>Rank: {data.rank}</Typography>
+                </Grid>
+
+                <Typography color="textSecondary">
+                  Price: {price} USD
+                </Typography>
+                <Typography color="textSecondary">Volume: {volume}</Typography>
+                <Typography color="textSecondary">
+                  Current Supply:{supply}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
+      })
+    ) : (
+      <div>Loading....</div>
+    );
+    return (
+      <Grid
+        container
+        direction="row"
+        spacing={10}
+        justify="center"
+        alignItems="center"
+      >
+        {dataList}
+      </Grid>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    data: state.data
+  };
+};
+
+export default connect(mapStateToProps, dataAction)(Data);
